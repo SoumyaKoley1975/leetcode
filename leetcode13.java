f257579f1498348b699ceecc912eac9b6f573bec
Soumya Koley <soumyakoley1234@gmail.com>
2026-08-01 01:06:02 +0530

    Add files via upload

---
+++ leetcode13.java
@@ -0,0 +1,80 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class leetcode13 {
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = map.get(s.charAt(i));
+
+            if (i < s.length() - 1 && current < map.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Roman Numeral: ");
+        String s = sc.nextLine();
+
+        int answer = romanToInt(s);
+
+        System.out.println("Integer Value: " + answer);
+
+        sc.close();
+    }
+}
+
+// leetcode format
+
+/*
+
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = map.get(s.charAt(i));
+
+            if (i < s.length() - 1 && current < map.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+}
+    
+*/
