f0ea0ec9ef5d65a7def1f7a579d71315b0e54366
Soumya Koley <soumyakoley1234@gmail.com>
2026-09-12 22:56:26 +0530

    leetcode 147

---
+++ leetcode147.java
@@ -0,0 +1,45 @@
+// leetcode solution 147
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        // Edge case: if the list is empty or has only one element
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // Dummy node acts as the start of the sorted list
+        ListNode dummy = new ListNode(0);
+        ListNode curr = head;
+        
+        while (curr != null) {
+            // Store the next node to process in the next iteration
+            ListNode nextNode = curr.next;
+            
+            // Start searching for the insertion point from the dummy node
+            ListNode prev = dummy;
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+            
+            // Insert curr between prev and prev.next
+            curr.next = prev.next;
+            prev.next = curr;
+            
+            // Move to the next node in the unsorted list
+            curr = nextNode;
+        }
+        
+        return dummy.next;
+    }
+}
+    
