b2eae51ca4dfceba9ebb02f9ac6a24df7e47707d
Soumya Koley <soumyakoley1234@gmail.com>
2026-09-17 23:26:56 +0530

    leetcode 64

---
+++ leetcode64.java
@@ -0,0 +1,28 @@
+// leetcode solution 64 
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        // Initialize the first row (can only move right)
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        
+        // Initialize the first column (can only move down)
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        
+        // Fill the rest of the grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        
+        // Bottom-right corner holds the minimum path sum
+        return grid[m - 1][n - 1];
+    }
+}
